dedfe93620099e0c095095f9b868f28bf728fe75
aps <aps@AMITT490>
2021-08-22 19:57:16 +0200

    Add project files.

---
+++ BusinessLogic/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    /// <summary>
+    /// Manage Product related methods.
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the price.
+        /// </summary>
+        /// <value>
+        /// The price.
+        /// </value>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Product"/> class.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        public Product(string id)
+        {
+            this.Id = id.ToUpper();
+
+            Dictionary<string, decimal> productPriceList = new Dictionary<string, decimal>();
+            productPriceList.Add("A", 50m);
+            productPriceList.Add("B", 30m);
+            productPriceList.Add("C", 20m);
+            productPriceList.Add("D", 15m);
+
+            if (productPriceList.ContainsKey(this.Id))
+            {
+                productPriceList.TryGetValue(this.Id, out decimal resultValue);
+                this.Price = resultValue;
+            }
+        }
+    }
+}
