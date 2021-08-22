c182bf28cb75035e912b1c9e28a026f75920ac5f
Amit Shah <amitshah4all@gmail.com>
2021-08-22 21:01:18 +0200

    Implement process order method

--- PromotionEngineClient/Program.cs
+++ PromotionEngineClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,55 @@ using System.Threading.Tasks;
 
 namespace PromotionEngineClient
 {
+    /// <summary>
+    /// Console application class to get product prices using promotion offers.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
+            ProcessOrders();
+        }
+
+        private static void ProcessOrders()
+        {
+            PromotionService objPromotion;
+            Product objProduct;
+            List<Product> products;
+            try
+            {
+                products = new List<Product>();
+                Console.WriteLine("Enter number of Products (only numeric values allowed):");
+
+                int noOfProducts = Convert.ToInt32(Console.ReadLine());
+                string type = string.Empty;
+                for (int i = 1; i <= noOfProducts; i++)
+                {
+                    Console.WriteLine($"Order {i}: Enter the type of product:A, B, C or D");
+                    type = Console.ReadLine();
+                    objProduct = new Product(type);
+                    products.Add(objProduct);
+                }
+
+                objPromotion = new PromotionService();
+                int totalPrice = objPromotion.GetTotalPrice(products);
+                Console.WriteLine($"Total Price: { totalPrice}");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                objPromotion = null;
+                objProduct = null;
+                products = null;
+            }
         }
     }
 }
