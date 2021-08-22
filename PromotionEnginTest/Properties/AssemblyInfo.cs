0ddd52880742c721c7c6849dd809914d097d82b4
Amit Shah <amitshah4all@gmail.com>
2021-08-22 20:13:21 +0200

    Add unit test project to solution and create one scenario.

---
+++ PromotionEnginTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PromotionEnginTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PromotionEnginTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4faa3a20-f57b-4ba3-8947-a7bcb446a545")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PromotionEnginTest/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace PromotionEnginTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        List<Product> productList;
+        PromotionService objPromotionService;
+
+        /// <summary>
+        /// Tests the initialize.
+        /// </summary>
+        [TestInitialize]
+        public void testInit()
+        {
+            productList = new List<Product>();
+            objPromotionService = new PromotionService();
+        }
+
+        /// <summary>
+        /// Tests the cleanup.
+        /// </summary>
+        [TestCleanup]
+        public void testCleanup()
+        {
+            productList = null;
+            objPromotionService = null;
+        }
+
+        [TestMethod]
+        public void Scenario1()
+        {
+
+            //Scenario A
+            //1 * A 50
+            //1 * B 30
+            //1 * C 20
+            //Total 100
+
+            productList = new List<Product>() { new Product("A"), new Product("B"), new Product("C") };
+            
+        }
+    }
+}
