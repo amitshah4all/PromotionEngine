04776a0958107a9f270c052f3f12a7108bec3482
Amit Shah <amitshah4all@gmail.com>
2021-08-22 20:31:38 +0200

    Implement error handling to method

--- BusinessLogic/PromotionService.cs
+++ BusinessLogic/PromotionService.cs
@@ -16,34 +16,41 @@ namespace BusinessLogic
         /// <exception cref="System.NotImplementedException"></exception>
         public int GetTotalPrice(List<Product> products)
         {
-            //Declaration of local variables 
-            int productCountA = 0, productPriceA = 50, productCountB = 0, productPriceB = 30,
-                productCountC = 0, productPriceC = 20, productCountD = 0, productPriceD = 15;
-
-            foreach (Product pr in products)
+            try
             {
-                switch (pr.Id.ToUpper())
-                {
-                    case "A":
-                        productCountA += 1;
-                        break;
-                    case "B":
-                        productCountB += 1;
-                        break;
-                    case "C":
-                        productCountC += 1;
-                        break;
-                    case "D":
-                        productCountD += 1;
-                        break;
-                }
-            }
+                //Declaration of local variables 
+                int productCountA = 0, productPriceA = 50, productCountB = 0, productPriceB = 30,
+                    productCountC = 0, productPriceC = 20, productCountD = 0, productPriceD = 15;
 
-            int totalPriceA = (productCountA / 3) * 130 + (productCountA % 3 * productPriceA);
-            int totalPriceB = (productCountB / 2) * 45 + (productCountB % 2 * productPriceB);
-            int totalPriceC = (productCountC * productPriceC);
-            int totalPriceD = (productCountD * productPriceD);
-            return totalPriceA + totalPriceB + totalPriceC + totalPriceD;
+                foreach (Product pr in products)
+                {
+                    switch (pr.Id.ToUpper())
+                    {
+                        case "A":
+                            productCountA += 1;
+                            break;
+                        case "B":
+                            productCountB += 1;
+                            break;
+                        case "C":
+                            productCountC += 1;
+                            break;
+                        case "D":
+                            productCountD += 1;
+                            break;
+                    }
+                }
+
+                int totalPriceA = (productCountA / 3) * 130 + (productCountA % 3 * productPriceA);
+                int totalPriceB = (productCountB / 2) * 45 + (productCountB % 2 * productPriceB);
+                int totalPriceC = (productCountC * productPriceC);
+                int totalPriceD = (productCountD * productPriceD);
+                return totalPriceA + totalPriceB + totalPriceC + totalPriceD;
+            }
+            catch (Exception ex)
+            {
+                throw ex;                
+            }
         }
     }
 }
