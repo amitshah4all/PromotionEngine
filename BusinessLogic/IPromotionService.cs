4ea71378235f569bb1897fc75cf4a99972e015ab
Amit Shah <amitshah4all@gmail.com>
2021-08-22 20:02:08 +0200

    Add  promotion interface and class

---
+++ BusinessLogic/IPromotionService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public interface IPromotionService
+    {
+        /// <summary>
+        /// Gets the type of the price by.
+        /// </summary>
+        /// <param name="product">The product.</param>
+        void GetProductPrice(Product product);
+
+        /// <summary>
+        /// Gets the total price.
+        /// </summary>
+        /// <param name="products">The products.</param>
+        /// <returns></returns>
+        int GetTotalPrice(List<Product> products);
+    }
+}
---
+++ BusinessLogic/PromotionService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    /// <summary>
+    /// Implement product service related methods
+    /// </summary>
+    public class PromotionService : IPromotionService
+    {
+        /// <summary>
+        /// Gets the type of the price by.
+        /// </summary>
+        /// <param name="product">The product.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public void GetProductPrice(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the total price.
+        /// </summary>
+        /// <param name="products">The products.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public int GetTotalPrice(List<Product> products)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
